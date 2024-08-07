28298381bb8bcd505841c1884346596118726d91
Simon Knight <127623348+YaxyoDiaz@users.noreply.github.com>
2024-08-07 12:53:01 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let car = {
+    mark:'TESLA',
+    model:'ModelY',
+    year: 2030
+}
+
+car.year = 2050
+
+car.color = 'black'
+
+let human = {
+    name: 'Jack',
+    age: 18
+}
+
+human.name = 'Doctor Doom'
+
+human.age = 24
+
+console.log(car);
+console.log(human);
